d7774e98999a70c4574a0479152ab5f288c46528
haripriyanka07 <haripriyanka.sunkari@zemosolabs.com>
2021-01-12 10:59:42 +0530

    worked on the routers maintaining the folder structure

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,28 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Home } from "./Components/Pages/Home";
-import { About } from "./Components/Pages/About";
-import Contact from "./Components/Pages/Contact";
-import Gallery from "./Components/Pages/Gallery";
-// import * as Reducer from './Components/atoms/reducer';
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import LoginButton from './Components/atoms/LoginButton';
 import LogoutButton from './Components/atoms/LogoutButton';
-import Add from "./Components/Pages/Add";
-import Remove from "./Components/Pages/Remove";
 import './index.css';
 import "./css/style.css";
 import { ToastContainer } from "react-toastify";
-
 import "react-toastify/dist/ReactToastify.css";
 import { Provider } from 'react-redux';
 import store from './Redux/rootReducer';
-// import { addAction, removeAction } from "./Redux/Image/imageActions";
-
-// export const imageContext = React.createContext()
+import Routes from "./Routes/routes";
 
 function AppRouter() {
-
-    // const [image, setImage] = useReducer(Reducer.reducer, Reducer.initialState)
     
     return (
         <Provider store={store}>
@@ -49,22 +37,13 @@ function AppRouter() {
                                 <li>
                                     <Link to="/remove/">Remove Image</Link>
                                 </li>
-                                {/* <li>
-                                    <Link to="/add/">Update Image</Link>
-                                </li> */}
+                                
                             </div>
                         </div>
                         <LoginButton />
                         <LogoutButton />
                     </nav>
-                    {/* <imageContext.Provider value={{image, setImage}}> */}
-                    <Route path="/" exact component={Home} />
-                    <Route path="/about/"  component={About} />
-                    <Route path="/contact/"  component={Contact} />
-                    <Route path="/gallery/"  component={Gallery} />
-                    <Route path="/add/" component={Add} />
-                    <Route path="/remove/" component={Remove} />
-                    {/* </imageContext.Provider> */}
+                    <Routes />
                     <ToastContainer position="bottom-right" autoClose={2000} hideProgressBar newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
                 </div>
             </Router>
